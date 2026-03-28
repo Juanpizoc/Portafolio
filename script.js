878aa79151bf3fa114402956f5c0f4968cdcc2e5
Juan Pizo <162081539+Juanpizoc@users.noreply.github.com>
2026-03-28 13:42:42 -0500

    cambio en js en inport

--- script.js
+++ script.js
@@ -2,28 +2,32 @@ if (!track || !next || !prev) {
   console.log("Error: elementos del carrusel no encontrados");
 }
 
-const track = document.querySelector('.carousel-track');
-const next = document.querySelector('.next');
-const prev = document.querySelector('.prev');
+document.addEventListener("DOMContentLoaded", () => {
 
-let index = 0;
+  const track = document.querySelector('.carousel-track');
+  const next = document.querySelector('.next');
+  const prev = document.querySelector('.prev');
 
-next.addEventListener('click', () => {
-  index++;
-  updateCarousel();
+  let index = 0;
+
+  next.addEventListener('click', () => {
+    index++;
+    updateCarousel();
+  });
+
+  prev.addEventListener('click', () => {
+    index--;
+    updateCarousel();
+  });
+
+  function updateCarousel() {
+    const cards = document.querySelectorAll('.project-card');
+    const width = 320;
+
+    if (index < 0) index = 0;
+    if (index >= cards.length) index = cards.length - 1;
+
+    track.style.transform = `translateX(-${index * width}px)`;
+  }
+
 });
-
-prev.addEventListener('click', () => {
-  index--;
-  updateCarousel();
-});
-
-function updateCarousel() {
-  const cards = document.querySelectorAll('.project-card');
-  const width = 320;
-
-  if (index < 0) index = 0;
-  if (index >= cards.length) index = cards.length - 1;
-
-  track.style.transform = `translateX(-${index * width}px)`;
-}
